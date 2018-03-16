27c4f00ae37fde4c6ecab5f4e9342d5ba45b7207
bethtelford <mrsbethtelford@gmail.com>
2018-03-16 09:06:09 -0600

    Further branch cleanup

--- db/schema.sql
+++
@@ -1,6 +0,0 @@
-create table products (
-    id serial unique primary key,
-    name varchar(20),
-    price money,
-    img text
-)
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { unregister } from './registerServiceWorker';
-import './reset.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
